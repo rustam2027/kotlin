a0ffe87f70271be4a3e1e8b736411883261c2c85
RustamSalimov <RustamSalimov@noreply.gitcode.com>
2026-09-17 11:55:02 +0700

    [DELETE?] Add some open mp symbols that are not listed

--- kotlin-native/libclangInterop/build.gradle.kts
+++ kotlin-native/libclangInterop/build.gradle.kts
@@ -69,6 +69,8 @@ nativeInteropPlugin {
                     "__ZN4llvm3omp23getLeafConstructsOrSelfENS0_9DirectiveE",
                     "__ZN4llvm7remarks14RemarkStreamerC1ENSt3__110unique_ptrINS0_16RemarkSerializerENS2_14default_deleteIS4_EEEENS2_8optionalINS_9StringRefEEE",
                     "__ZN4llvm3omp17getLeafConstructsENS0_9DirectiveE",
+                    "__ZN4llvm3omp15isLeafConstructENS0_9DirectiveE",
+                    "__ZN4llvm3omp40getOpenMPContextTraitPropertyForSelectorENS0_13TraitSelectorE",
                     "__ZN4llvm15OpenMPIRBuilder25getOpenMPDefaultSimdAlignERKNS_6TripleERKNS_9StringMapIbNS_15MallocAllocatorEEE",
                     "__ZN4llvm4hlsl7rootsig16dumpRootElementsERNS_11raw_ostreamENS_8ArrayRefINSt3__17variantIJNS_4dxbc9RootFlagsENS1_13RootConstantsENS1_14RootDescriptorENS1_15DescriptorTableENS1_21DescriptorTableClauseENS1_13StaticSamplerEEEEEE"
             ).mapTo(this) { "-Wl,-U,$it" }
